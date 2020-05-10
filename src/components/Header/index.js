eb75f4f79e6c7483439f554a7b6a785391ee2bff
Skyler Tosh <skylerjtosh@gmail.com>
2020-05-09 19:07:35 -0700

    Added header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+	return(
+		<header>
+			<i class="fas fa-book"></i>
+			<h1>Google Book Search</h1>
+		</header>
+	)
+}
+
+export default Header;
